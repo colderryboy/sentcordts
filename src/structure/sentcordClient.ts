ebb14b37d2e5b923aa9dbd39bbc72f71f7cce4e4
Colderry <72723052+Colderry@users.noreply.github.com>
2021-05-19 03:58:49 +0600

    sentcordClient.ts

---
+++ src/structure/sentcordClient.ts
@@ -0,0 +1,65 @@
+import fetch from "node-fetch";
+import { Logger } from "@ayanaware/logger";
+
+export default class sentCordClient {
+    public apikey: String;
+    public id: String;
+
+    constructor(options = {
+        token: "",
+        id: ""
+    }) {
+        /**
+         * The api key of the bot from sentcord
+         * @type {apikey}
+         */
+
+        this.apikey = options.token;
+
+        /**
+         * The id of the bot
+         * @type {id}
+         */
+        this.id = options.id;
+
+    };
+    /**
+     * 
+     * @param servers Total servers of the bot
+     * @param shards Total shards of the bot
+     */
+    async post(servers?: String, shards?: String) {
+        if (!this.apikey) throw new ReferenceError("Api key is missing");
+        if (!this.id) throw new ReferenceError("ID is missing");
+        /**
+         * If servers not provided then 0 by default
+         */
+        if (!servers) servers = "0";
+        /**
+         * If shards not provided then 0 by default
+         */
+        if (!shards) shards = "0";
+
+        await fetch(`https://sentcord.com/api/bot/${this.id}`, {
+            method: "POST",
+            headers: {
+                "Authorization": `${this.apikey}`,
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                serverCount: servers,
+                shardCount: shards
+            })
+        });
+    };
+    /**
+     * 
+     * @param text Nothing special just for logging custom text
+     */
+    log(text?: String) {
+        if (!text) text = "Sentcordts is ready";
+
+        var log = Logger.get();
+        log.info(`${text}`);
+    };
+};
